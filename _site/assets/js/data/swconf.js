179e10dd2519c6850aa64179bcf42a0a01fb77cd
Tristan van Onselen <Tristanvonselen@gmail.com>
2025-03-22 06:07:45 +0000

    fix: update site URL and modify published times in metadata for consistency

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1742503362',resources: [
+    cacheName: 'chirpy-1742623276',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
